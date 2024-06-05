43b0bcd77434b58f9c766587734ae68177ae751e
davignz <werdavid1@gmail.com>
2024-06-04 23:10:04 -0300

    adicionar menu responsivo em todas as páginas, finalizar estilos e ajustes.
    Co-authored-by:  Jennifer Daleffi  <JenniferDaleffi@users.noreply.github.com>
    Co-authored-by: PauloHenrique63 <PauloHenrique63@users.noreply.github.com>

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,26 @@
 const menuIcon = document.querySelector(".fa-bars");
 const menu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".fa-xmark");
+
+let menuAberto = false;
 
 menuIcon.addEventListener("click", () => {
+  menuAberto = !menuAberto;
+
   menu.classList.toggle("active");
+  menuIcon.classList.remove("fa-bars", "fa-xmark");
+
+  if (menuAberto) {
+    menuIcon.classList.add("fa-xmark");
+  } else {
+    menuIcon.classList.add("fa-bars");
+  }
 });
+
+if (closeMenu) {
+  closeMenu.addEventListener("click", () => {
+    menuIcon.click();
+  });
+}
+
+const menuLinks = menu.querySelectorAll("a");
